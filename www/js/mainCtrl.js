7a21441697fdd5bb8c5591e70c690e1e11dd8359
Jordan <jordanthomp81@gmail.com>
2015-07-09 15:00:50 -0500

    asfda

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -1,4 +1,24 @@
 angular.module('starter')
-  .controller('mainCtrl', ['$scope', function($scope){
-
+  .controller('mainCtrl', ['$scope', '$interval', function($scope, $interval){
+      $scope.minutes = $scope.seconds = 0;
+      $scope.convertToMinutes = function(value) {
+         $scope.minutes = Math.floor(value / 60);
+         $scope.seconds = value % 60;
+      };
+      
+      $scope.startTimer = function() {
+        $interval(function() {
+            if ( $scope.minutes != 0 || $scope.seconds != 0 ) {
+                if ( $scope.seconds != 0 ) {
+                    $scope.seconds = $scope.seconds - 1;
+                    console.log($scope.seconds);
+                }else {
+                    $scope.seconds = 60;
+                    $scope.minutes -= 1;
+                } 
+            }else {
+                // $cordovaVibration.vibarate(100);   
+            }
+        }, 1000);  
+      };
   }]);
