25307f12f69e82fbdfe665e55d9e32ba183a15c4
Jordan <jordanthomp81@gmail.com>
2015-07-21 14:00:38 -0500

    Made style changes; Updated timer range to move according to the displayed time

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -1,24 +1,35 @@
 angular.module('starter')
+
   .controller('mainCtrl', ['$scope', '$interval', function($scope, $interval){
+      var countdown = null;
       $scope.minutes = $scope.seconds = 0;
+      $scope.totalSeconds = 0;
+      
       $scope.convertToMinutes = function(value) {
          $scope.minutes = Math.floor(value / 60);
          $scope.seconds = value % 60;
+         $scope.totalSeconds = value;
       };
       
       $scope.startTimer = function() {
-        $interval(function() {
+        countdown = $interval(function() {
             if ( $scope.minutes != 0 || $scope.seconds != 0 ) {
                 if ( $scope.seconds != 0 ) {
                     $scope.seconds = $scope.seconds - 1;
-                    console.log($scope.seconds);
+                    $scope.totalSeconds -= 1;
+                    $('.timer-slider').val($scope.totalSeconds)
                 }else {
                     $scope.seconds = 60;
                     $scope.minutes -= 1;
                 } 
             }else {
-                // $cordovaVibration.vibarate(100);   
+                // $cordovaVibration.vibrate(100);
+                $interval.cancel(countdown);
             }
         }, 1000);  
+      };
+      
+      $scope.stopTimer = function() {
+          $interval.cancel(countdown);
       };
   }]);
